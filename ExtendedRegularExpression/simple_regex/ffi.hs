bdfdd6e32b8db0c3caedaab302467b8704754287
henry-ly <lyhenry94@gmail.com>
2019-09-18 12:25:30 +0200

    more notes

--- ExtendedRegularExpression/simple_regex/ffi.hs
+++ ExtendedRegularExpression/simple_regex/ffi.hs
@@ -4,6 +4,7 @@ module FFI where
 {- In order to create a working executable
 gcc simple_re_match.c -o simple_re_match 
 ghc --make -main-is FFI ffi.hs simple_re_match.c
+note to self: ghci ffi.hs simple_re_match.o
 -}
 
 import Foreign.C.Types 
@@ -15,6 +16,14 @@ foreign import ccall "match"
      c_match :: CString -> CString -> CInt
 match :: CString -> CString -> CInt
 match a b = c_match a b
+
+matcher :: String -> String -> IO CInt
+matcher a b = do 
+              newRegex <- newCString a
+              newString <- newCString b
+              return $ match newRegex newString
+
+
 main = do 
   newC <- newCString "Hello"
   putStrLn $ show(match newC newC)
