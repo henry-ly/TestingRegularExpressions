4212dc5ab01766a47cbfeb57db616fd70932ce81
henry-ly <lyhenry94@gmail.com>
2019-10-07 11:57:41 +0200

    tests for continuation-based matcher

--- DefunctionalizationAtWork/RegExp.hs
+++ DefunctionalizationAtWork/RegExp.hs
@@ -1,6 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 import GHC.Exts (IsString(..))
 import Test.QuickCheck
+-- for unix grep
+import System.Process 
+import System.Exit
+import System.IO.Unsafe
+import Test.QuickCheck.Monadic
+import Control.Monad.IO.Class
+import Control.Monad(liftM, liftM2)
+
+import FEAT
 
 data Regexp = Zero                  -- empty
             | One                   -- epsilon
@@ -10,8 +19,19 @@ data Regexp = Zero                  -- empty
             | Many Regexp           -- repetition (*)
             deriving Show
 
+
 infixl 6 <+>
 infixl 7 <>
+
+space :: Regexp -> Space String
+space (Lit c)  = pay (unit [c])
+space Zero       = empty
+space One       = unit ""
+space (p `Plus` q) = space p +++ space q
+space (p `Cat` q) = unit (++) `app` space p `app` space q
+space (Many p)  = w
+ where
+  w = unit "" +++ pay (unit (++) `app` unpay (space p) `app` w)
 
 (<+>) :: Regexp -> Regexp -> Regexp
 Zero <+> e = e
@@ -58,7 +78,7 @@ instance IsString Regexp where
 
 
 alphabet :: Gen Char
-alphabet = elements ['a', 'b', 'c']
+alphabet = elements ['a' .. 'c']
 
 literal = do  
           x <- alphabet
@@ -93,70 +113,168 @@ arbitraryExpression n = frequency[(1, regexPlusGen subexpr subexpr)
                                  ]
                           where subexpr = arbitraryExpression (n `div` 2)
 
-prop_zero :: String -> Bool
-prop_zero s = match Zero s == null s
+unixGrep :: String -> String -> IO Bool
+unixGrep s r = do  
+            exitCode <- system $ "echo \"" ++ s ++ "\" | grep -E -w -q \"" ++ r ++"\""
+            case exitCode of
+                ExitSuccess ->  return True
+                _           ->  return False
 
-prop_one :: String -> Bool
-prop_one s = match One s == null s
+-- convert a regular expression to UNIX regex in a string representation
+showExpr :: Regexp -> String
+showExpr One = []
+showExpr (Lit a) = [a] 
+showExpr (Many a) = "(" ++ showExpr a ++ ")" ++ "*" 
+showExpr (Plus a b) = "(" ++ showExpr a ++"|"++ showExpr b ++ ")" 
+showExpr (Cat a b) = showExpr a ++ showExpr b
+showExpr _ = "a&&b"
+
+
+prop_Grep :: Regexp -> Property
+prop_Grep r1 =  monadicIO  $ do  
+                             testString <-  run(generate(genInputStrings 10 r1))
+                             booleanGrep <- run(unixGrep testString (showExpr r1))
+                             monitor (counterexample testString)
+                             assert $ match r1 testString == booleanGrep
+
+prop_Nil :: Property
+prop_Nil = monadicIO $ do
+                       testString <- run(generate(genInputStrings 10 Zero)) 
+                       monitor(counterexample testString)
+                       assert $ match Zero testString == False
+
+prop_Eps :: Property
+prop_Eps = monadicIO $ do
+                       testString <- run(generate(genInputStrings 10 One))
+                       monitor(counterexample testString)
+                       assert $ match One testString == null testString
+
+prop_Atom :: Char -> Property
+prop_Atom a = monadicIO $ do
+                          testString <- run(generate(genInputStrings 10 (Lit a)))
+                          monitor(counterexample testString)
+                          assert $ match (Lit a) testString == (testString == [a])
+
+prop_Plus :: Regexp -> Regexp -> Property
+prop_Plus r1 r2 = monadicIO $ do
+                              testString <- run(generate(genInputStrings 10 r1))
+                              monitor(counterexample testString)
+                              assert $ match (r1 `Plus` r2) testString == match r1 testString || match r2 testString
+
+prop_Seq :: Regexp -> Regexp -> Property
+prop_Seq r1 r2 = monadicIO $ do
+                             testString <- run(generate(genInputStrings 10 r1))
+                             monitor(counterexample testString)
+                             assert $ match (r1 `Cat` r2) testString ==
+                               or[match r1 (take i testString) && match r2 (drop i testString) | i <- [0 .. length testString]]
+
+
+prop_Clo :: Regexp -> Property
+prop_Clo r1 = monadicIO $ do
+                          testString <- run(generate(genInputStrings 10 r1))
+                          monitor(counterexample testString)
+                          assert $ match (Many r1) testString == null testString || (match (r1 `Cat` Many r1) testString)
+
+
+-- will be called from properties
+testMatcher ::  Regexp -> Regexp -> Property
+testMatcher r1 r2 = monadicIO  $ do
+                                 testString <- run(generate(genInputStrings 10 r1))
+                                 monitor(counterexample testString)
+                                 assert $ match r1 testString == match r2 testString
 
 --Alternation Laws
-prop_AltAssoc :: Regexp -> Regexp -> Regexp -> String -> Bool
-prop_AltAssoc a b c s = match (Plus a (Plus b c)) s == match (Plus (Plus a b) c) s
+prop_AltAssoc :: Regexp -> Regexp -> Regexp -> Property
+prop_AltAssoc a b c = testMatcher (a `Plus` (b `Plus` c)) ((a `Plus` b) `Plus` c) 
 
-prop_AltCom :: Regexp -> Regexp -> String -> Bool
-prop_AltCom a b s = match (Plus a b) s == match (Plus b a) s
+prop_AltCom :: Regexp -> Regexp -> Property
+prop_AltCom a b = testMatcher (a `Plus` b) (b `Plus` a)
 
-prop_AltIdem :: Regexp -> String -> Bool
-prop_AltIdem a s = match (Plus a a) s == match a s
-
-prop_AltIden :: Regexp -> String -> Bool
-prop_AltIden a s = match (Plus a Zero) s == match a s
+prop_AltIdem :: Regexp -> Property
+prop_AltIdem a = testMatcher (a `Plus` a) a 
 
 --Concatenation Laws
-prop_CatAssoc :: Regexp -> Regexp -> Regexp -> String -> Bool
-prop_CatAssoc a b c s = match (Cat a (Cat b c)) s == match (Cat (Cat a b) c) s
+prop_CatAssoc :: Regexp -> Regexp -> Regexp -> Property
+prop_CatAssoc a b c = testMatcher (a `Cat` (b `Cat` c))  ((a `Cat` b) `Cat` c) 
 
-prop_CatIden :: Regexp -> String -> Bool
-prop_CatIden a s = match (Cat a Zero) s == match a s
 
-prop_DistLeft :: Regexp -> Regexp -> Regexp -> String -> Bool
-prop_DistLeft a b c s = match (Cat a (Plus b c)) s == match (Plus (Cat a b) (Cat a c)) s
+prop_DistLeft :: Regexp -> Regexp -> Regexp -> Property
+prop_DistLeft a b c = testMatcher (a `Cat` (b `Plus` c)) ((a `Cat` b) `Plus` (a `Cat` c))
 
-prop_DistRight :: Regexp -> Regexp -> Regexp -> String -> Bool
-prop_DistRight a b c s = match (Cat (Plus a b) c) s == match (Plus (Cat a c)(Cat b c)) s
+prop_DistRight :: Regexp -> Regexp -> Regexp -> Property
+prop_DistRight a b c = testMatcher ((a `Plus` b) `Cat` c) ((a `Cat` c) `Plus` (b `Cat` c))
 
 --Closure Properties
-prop_Clo :: Regexp -> String -> Bool
-prop_Clo a s = match (Many (Many a)) s == match (Many a) s
+prop_Clo2 :: Regexp -> Property
+prop_Clo2 a = testMatcher (Many (Many a)) (Many a) 
 
-prop_Clo2 :: Regexp -> String -> Bool
-prop_Clo2 a s = match (Plus One (Cat a (Many a))) s == match (Many a) s
+prop_Clo3 :: Regexp -> Property
+prop_Clo3 a = testMatcher (One `Plus` (a `Cat` (Many a))) (Many a)
 
-deepCheck 1 p = quickCheckWith (stdArgs {maxSuccess = 10000}) p
+
+
+genInputStrings :: Int -> Regexp -> Gen String
+genInputStrings s r = oneof[genMatching s r, genNotMatching s] 
+
+genMatching :: Int -> Regexp -> Gen String
+genMatching s r = if not (null (space r)) then
+                      oneof[do 
+                               a <- choose(0, n-1)
+                               if n == 0 then
+                                   return ""
+                               else
+                                   return(h a) 
+                           |(sz, (n,h)) <- [0..s] `zip` space r] 
+                           else
+                               return ""
+-- pseudo non-matching string will generate strings at random
+genNotMatching :: Int -> Gen String
+genNotMatching n = do
+                   r <- choose (0, n) 
+                   vectorOf r alphabet 
+
+-- enumerates matching strings up to size s given a regex r
+validStrings s r = concat [enum(n, h)|(sz, (n,h)) <- [0..s] `zip` space r ] 
+
+
+deepCheck 1 p = quickCheckWith (stdArgs {maxSuccess = 100, maxSize = 30}) p
 deepCheck n p = do
-                quickCheckWith (stdArgs {maxSuccess = 10000}) p
+                quickCheckWith (stdArgs {maxSuccess = 100, maxSize = 30}) p
                 deepCheck (n-1) p
 main = do
+       putStrLn "prop_Nil"
+       deepCheck iterations prop_Nil
+       putStrLn "prop_Eps"
+       deepCheck iterations prop_Eps
+       putStrLn "prop_Atom"
+       deepCheck iterations prop_Atom
+       putStrLn "prop_Plus"
+       deepCheck iterations prop_Plus
+       putStrLn "prop_Seq"
+       deepCheck iterations prop_Seq
+        
+
+       putStrLn "prop_Grep:"
+       deepCheck iterations prop_Grep
        putStrLn "prop_AltAssoc:"
        deepCheck iterations prop_AltAssoc
        putStrLn "prop_AltCom:"
        deepCheck iterations prop_AltCom
        putStrLn "prop_AltIdem:"
        deepCheck iterations prop_AltIdem
-       putStrLn "prop_AltIden:"
-       deepCheck iterations prop_AltIden
        putStrLn "prop_CatAssoc:"
        deepCheck iterations prop_CatAssoc
-       putStrLn "prop_CatIden:"
-       deepCheck iterations prop_CatIden
        putStrLn "prop_DistLeft"
        deepCheck iterations prop_DistLeft
        putStrLn "prop_DistRight"
-       deepCheck iterations prop_DistRight
-       putStrLn "prop_Clo"
-       deepCheck iterations prop_Clo
+       deepCheck iterations prop_DistRight     
+       putStrLn "prop_Clo1"
+       deepCheck iterations prop_Clo     
        putStrLn "prop_Clo2"
-       deepCheck iterations prop_Clo2
-       putStrLn "End"
-       where iterations = 100
+       deepCheck iterations prop_Clo2     
+       putStrLn "prop_Clo3"
+       deepCheck iterations prop_Clo3     
+
+       putStrLn "END"
+       where iterations = 500
 
---
+++ DefunctionalizationAtWork/test.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+ghc RegExp.hs
+
+echo "Testing RegExp.hs..."
+
+./RegExp > result.txt
+
+#extract and count failed and passed tests
+echo "prop_Nil"
+echo "Successful: $(awk '/prop_Nil/,/prop_Eps/' result.txt | grep -c "OK") Failed: $(awk '/prop_Nil/,/prop_Eps/' result.txt | grep -c "Failed")"
+echo
+echo "prop_Eps"
+echo "Successful: $(awk '/prop_Eps/,/prop_Atom/' result.txt | grep -c "OK") Failed: $(awk '/prop_Eps/,/prop_Atom/' result.txt | grep -c "Failed")"
+echo
+echo "prop_Atom"
+echo "Successful: $(awk '/prop_Atom/,/prop_Plus/' result.txt | grep -c "OK") Failed: $(awk '/prop_Atom/,/prop_Plus/' result.txt | grep -c "Failed")"
+echo
+echo "prop_Plus"
+echo "Successful: $(awk '/prop_Plus/,/prop_Seq/' result.txt | grep -c "OK") Failed: $(awk '/prop_Plus/,/prop_Seq/' result.txt | grep -c "Failed")"
+echo
+echo "prop_Seq"
+echo "Successful: $(awk '/prop_Seq/,/prop_Grep/' result.txt | grep -c "OK") Failed: $(awk '/prop_Seq/,/prop_Grep/' result.txt | grep -c "Failed")"
+echo
+echo "prop_Grep"
+echo "Successful: $(awk '/prop_Grep/,/prop_AltAssoc/' result.txt | grep -c "OK") Failed: $(awk '/prop_Grep/,/prop_AltAssoc/' result.txt | grep -c "Failed")"
+echo
+echo "prop_AltAssoc"
+echo "Succesful: $(awk '/prop_AltAssoc/,/prop_AltCom/' result.txt | grep -c "OK") Failed: $(awk '/prop_AltAssoc/,/prop_AltCom/' result.txt | grep -c "Failed")"
+echo
+echo "prop_AltCom"
+echo "Succesful: $(awk '/prop_AltCom/,/prop_AltIdem/' result.txt | grep -c "OK") Failed: $(awk '/prop_AltCom/,/prop_AltIdem/' result.txt | grep -c "Failed")"
+echo
+echo "prop_AltIdem"
+echo "Succesful: $(awk '/prop_AltIdem/,/prop_CatAssoc/' result.txt | grep -c "OK") Failed: $(awk '/prop_AltIdem/,/prop_CatAssoc/' result.txt | grep -c "Failed")"
+echo
+echo "prop_CatAssoc"
+echo "Succesful: $(awk '/prop_CatAssoc/,/prop_DistLeft/' result.txt | grep -c "OK") Failed: $(awk '/prop_CatAssoc/,/prop_DistLeft/' result.txt | grep -c "Failed")"
+echo
+echo "prop_DistLeft"
+echo "Succesful: $(awk '/prop_DistLeft/,/prop_DistRight/' result.txt | grep -c "OK") Failed: $(awk '/prop_DistLeft/,/prop_DistRight/' result.txt | grep -c "Failed")"
+echo
+echo "prop_DistRight"
+echo "Succesful: $(awk '/prop_DistRight/,/prop_Clo/' result.txt | grep -c "OK") Failed: $(awk '/prop_DistRight/,/prop_Clo/' result.txt | grep -c "Failed")"
+echo
+echo "prop_Clo1"
+echo "Succesful: $(awk '/prop_Clo1/,/prop_Clo2/' result.txt | grep -c "OK") Failed: $(awk '/prop_Clo1/,/prop_Clo2/' result.txt | grep -c "Failed")"
+echo
+echo "prop_Clo2"
+echo "Succesful: $(awk '/prop_Clo2/,/prop_Clo3/' result.txt | grep -c "OK") Failed: $(awk '/prop_Clo2/,/prop_Clo3/' result.txt | grep -c "Failed")"
+echo
+echo "prop_Clo3"
+echo "Succesful: $(awk '/prop_Clo3/,/END/' result.txt | grep -c "OK") Failed: $(awk '/prop_Clo3/,/END/' result.txt | grep -c "Failed")"
